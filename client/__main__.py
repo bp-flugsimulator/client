2f2aea0b01a2bf5700d73bcbd414764234228038
jonas <jonas.meurer@outlook.de>
2017-12-10 16:19:00 +0100

    fixed main function

--- client/__main__.py
+++ client/__main__.py
@@ -1,26 +1,39 @@
+"""
+Main file
+"""
+
 import argparse
 import asyncio
+import sys
+
 from utils import Status, Command, RpcReceiver
 
 
 def run():
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument(
-        'host', metavar='H', type=str, nargs='1', help='ip address to server')
+        'host',
+        metavar='H',
+        type=str,
+        help='ip address to server',
+    )
     parser.add_argument(
-        'port', metavar='P', type=int, nargs='1', help='a network port')
+        'port',
+        metavar='P',
+        type=int,
+        help='a network port',
+    )
 
     args = parser.parse_args()
 
-    url_listen = args.host + ":" + args.port + "/commands"
-    url_send = args.host + ":" + args.port + "/notifications"
+    url_listen = args.host + ":" + str(args.port) + "/commands"
+    url_send = args.host + ":" + str(args.port) + "/notifications"
 
     try:
         print("Starting client ...")
         rpc = RpcReceiver(url_listen, url_send)
         asyncio.get_event_loop().run_until_complete(rpc.run())
         print("Finished client ...")
-
     except:
         pass
 
