8d150d5eb29c69e04c722154188d98b0c74c5c92
Tim <tim.weissmantel@stud.tu-darmstadt.de>
2018-01-11 16:29:22 +0100

    fixed tests

--- setup.py
+++ setup.py
@@ -14,9 +14,9 @@ def get_requirements(file):
     -------
         An array of dependencies.
     """
-
-    return parse_requirements(file, session="r")
-
+    install_reqs = parse_requirements(file, session="r")
+    install_reqs_list = [str(ir.req) for ir in install_reqs]
+    return filter(lambda x: x is not None, install_reqs_list) 
 
 setup(
     name="bp-flugsimulator-client",
