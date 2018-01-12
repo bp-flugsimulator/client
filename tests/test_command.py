0f6853ba54a0fbc6f7f67fe784231fa0cc6972ca
Tim <tim.weissmantel@stud.tu-darmstadt.de>
2018-01-12 21:30:24 +0100

    fixed test for windows

--- tests/test_command.py
+++ tests/test_command.py
@@ -123,9 +123,11 @@ class TestCommands(unittest.TestCase):
         if os.name == 'nt':
             prog = "C:\\Windows\\System32\\cmd.exe"
             args = ["/c", "SLEEP 10"]
+            exit_code = 1
         else:
             prog = "/bin/sh"
             args = ["-c", "sleep 10"]
+            exit_code = -15
 
         @asyncio.coroutine
         def create_and_cancel_task():
@@ -135,4 +137,6 @@ class TestCommands(unittest.TestCase):
             result = yield from task
             return result
 
-        self.assertEqual(-15, self.loop.run_until_complete(create_and_cancel_task()))
+        self.assertEqual(exit_code,
+                         self.loop.run_until_complete(
+                             create_and_cancel_task()))
