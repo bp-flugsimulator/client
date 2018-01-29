b47774ce4a55274db8fc7000a67818c1c882edce
Tim <tim.weissmantel@stud.tu-darmstadt.de>
2018-01-29 20:24:33 +0100

    possible appveyor fix 2. try

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -34,15 +34,16 @@ class TestCommands(TestCase):
 
     def test_multiple_preexisting_logfolders(self):
         dir_1 = datetime.now().strftime(ClientLogger.DATE_FORMAT)
-        sleep(0.1)
+        sleep(0.01)
         dir_2 = datetime.now().strftime(ClientLogger.DATE_FORMAT)
-        sleep(0.1)
+        sleep(0.01)
         dir_3 = datetime.now().strftime(ClientLogger.DATE_FORMAT)
 
         mkdir(join(self.FOLDER, dir_1))
         mkdir(join(self.FOLDER, dir_2))
         mkdir(join(self.FOLDER, dir_3))
 
+        sleep(0.01)
         logger.LOGGER.enable()
         logger.LOGGER.disable()
 
@@ -55,13 +56,14 @@ class TestCommands(TestCase):
     def test_unknown_preexisting_logfolders(self):
         dir_unknown = 'unknown'
         dir_1 = datetime.now().strftime(ClientLogger.DATE_FORMAT)
-        sleep(0.1)
+        sleep(0.01)
         dir_2 = datetime.now().strftime(ClientLogger.DATE_FORMAT)
 
         mkdir(join(self.FOLDER, dir_1))
         mkdir(join(self.FOLDER, dir_2))
         mkdir(join(self.FOLDER, dir_unknown))
 
+        sleep(0.01)
         logger.LOGGER.enable()
         logger.LOGGER.disable()
 
