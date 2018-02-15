0e3781e64b17b81dee25422e3c6f108a5d202a32
Tim <tim.weissmantel@stud.tu-darmstadt.de>
2018-02-15 23:10:22 +0100

    tee now has a max logfile size

--- applications/tee.py
+++ applications/tee.py
@@ -3,14 +3,22 @@ from sys import stdin, stdout
 
 parser = ArgumentParser()
 parser.add_argument('--path', type=str, help='path to logfile')
+parser.add_argument('--max-size', type=int, default=1048576,help='max size of the logfile in bytes')
+parser.add_argument('--chunk-size', type=int, default=1024,help='max size of a chunk in bytes')
 args = parser.parse_args()
 
-with open(args.path, mode='w') as logfile:
+with open(args.path, mode='wb+') as logfile:
+    while True:
+        buffer = stdin.buffer.read(1)
+        if buffer.decode() is '':
+            break
+        elif logfile.tell() >= args.max_size:
+            logfile.seek(args.chunk_size)
+            tmp = logfile.read()
+            logfile.seek(0)
+            logfile.write(tmp)
 
-    buffer = stdin.read(1)
-    while buffer is not '':
         logfile.write(buffer)
         logfile.flush()
-        stdout.write(buffer)
-        stdout.flush()
-        buffer = stdin.read(1)
+        stdout.buffer.write(buffer)
+        stdout.buffer.flush()
