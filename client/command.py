7e3773f177d5902b4fa8011a7d17f8f607094427
jonas <jonas.meurer@outlook.de>
2018-02-22 12:32:02 +0100

    fixed remove_trailing_seperator

--- client/command.py
+++ client/command.py
@@ -59,7 +59,7 @@ def remove_trailing_path_seperator(path):
     -------
         string
     """
-    if path and path[-1] == os.path.sep:
+    if path and (path[-1] == '\\' or path[-1] == '/'):
         return path[:-1]
     else:
         return path
